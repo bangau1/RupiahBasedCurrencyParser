3240aaa364ad88211ec5d44e0599bf37ebd9df22
root <root@h180-147-243-108.vps.ablenet.jp>
2014-03-29 13:22:24 +0900

    fixed worker

--- worker.py
+++ worker.py
@@ -1,15 +1,15 @@
-#!/bin/py
+#!/usr/bin/python
 from models import BankCurrencyDB, BankCurrencyInfo
 from parsers import BCAParser, BNIParser, CIMBNiagaParser, BankMandiriParser
 import time
 def initBankDB(filepath):
 	return BankCurrencyDB(filepath)
 def getDefaultDatabasePath():
-	return "database/currency.json";
+	return "/var/www/products/RupiahBasedCurrencyParser/database/currency.json";
 def getDatabasePathDaily(now=time.time()):
 	jakartaEpochTime = getAsiaJakartaEpoch(now)
 	jakartaStructTime = time.gmtime(jakartaEpochTime)
-	return "database/{year:0>4}/{month:0>2}/currency_{date:0>2}.json".format(year=jakartaStructTime.tm_year, month=jakartaStructTime.tm_mon, date=jakartaStructTime.tm_mday);
+	return "/var/www/products/RupiahBasedCurrencyParser/database/{year:0>4}/{month:0>2}/currency_{date:0>2}.json".format(year=jakartaStructTime.tm_year, month=jakartaStructTime.tm_mon, date=jakartaStructTime.tm_mday);
 def getAsiaJakartaEpoch(now=time.time()):
 	return now +  7*3600;
 if __name__ == "__main__":
